a3a5a33b6c719ea714ffadb62795c4aa2ec9cc7a
JungKangWoo <todendvv91@gmail.com>
2023-06-13 09:29:53 +0900

    update

--- src/components/CarouselSlick.jsx
+++ src/components/CarouselSlick.jsx
@@ -1,4 +1,4 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, Text } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,38 +12,47 @@ export default function CarouselSlick() {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
-
+  const image = [
+    {
+      title: "AVENGERS",
+      image: "https://wallpapers.com/images/featured/bte9zcsa9pvyzpvk.jpg",
+    },
+    {
+      title: "END GAME",
+      image: "https://wallpaperset.com/w/full/a/c/7/369420.jpg",
+    },
+    {
+      title: "AVENGERS: THE INITIATIVE #1",
+      image: "https://cdn.wallpapersafari.com/41/85/JgLroC.jpg",
+    },
+    {
+      title: "IT'S THE AVENGERS",
+      image: "https://images.hdqwalls.com/wallpapers/marvel-4k-artwork-ep.jpg",
+    },
+  ];
   return (
     <Box w={"full"} h={"full"}>
       <Slider {...settings}>
-        <HStack
-          w={"full"}
-          h={"450px"}
-          backgroundImage={`url("https://wallpapers.com/images/featured/bte9zcsa9pvyzpvk.jpg")`}
-          backgroundSize={"cover"}
-          backgroundPosition={"center"}
-        ></HStack>
-        <HStack
-          w={"full"}
-          h={"450px"}
-          backgroundImage={`url("https://wallpaperset.com/w/full/a/c/7/369420.jpg")`}
-          backgroundSize={"cover"}
-          backgroundPosition={"center"}
-        ></HStack>
-        <HStack
-          w={"full"}
-          h={"450px"}
-          backgroundImage={`url("https://cdn.wallpapersafari.com/41/85/JgLroC.jpg")`}
-          backgroundSize={"cover"}
-          backgroundPosition={"center"}
-        ></HStack>{" "}
-        <HStack
-          w={"full"}
-          h={"450px"}
-          backgroundImage={`url("https://images.hdqwalls.com/wallpapers/marvel-4k-artwork-ep.jpg")`}
-          backgroundSize={"cover"}
-          backgroundPosition={"center"}
-        ></HStack>
+        {image.map((item) => (
+          <Box w={"full"} justifyContent={"center"}>
+            <HStack
+              w={"full"}
+              h={"450px"}
+              backgroundImage={item.image}
+              backgroundSize={"cover"}
+              backgroundPosition={"center"}
+            >
+              <Text
+                w={"full"}
+                fontSize={"80px"}
+                fontWeight={800}
+                textAlign={"center"}
+              >
+                {item.title}
+              </Text>
+            </HStack>
+          </Box>
+        ))}
       </Slider>
     </Box>
   );
